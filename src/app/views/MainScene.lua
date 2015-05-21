11e58098509d0dac33ea75992736f7d360ac4a9a
Naoyuki Totani <nyk.totani@gmail.com>
2015-05-22 00:40:32 +0900

    minimum template

--- src/app/views/MainScene.lua
+++ src/app/views/MainScene.lua
@@ -1,7 +1,12 @@
+local jam = require("app.jam")
 
 local MainScene = class("MainScene", cc.load("mvc").ViewBase)
 
 function MainScene:onCreate()
+    display.newLayer():addTo(self):onTouch(function(e)
+        print(e.x, e.y)
+    end)
 end
 
 return MainScene
+
