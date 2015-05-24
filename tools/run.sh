d85d825392727d1ef22cc9e654324a2deb945d25
Naoyuki Totani <nyk.totani@gmail.com>
2015-05-24 16:10:38 +0900

    fix tools

--- tools/run.sh
+++ tools/run.sh
@@ -1,1 +1,1 @@
-open runtime/mac/PrebuiltRuntimeLua.app --args -workdir /Users/ntotani/Documents/cocos/MyLuaGame -debugger codeide -scale 1.5
+open runtime/mac/PrebuiltRuntimeLua.app --args -workdir /Users/ntotani/Documents/cocos/MyLuaGame -debugger codeide
